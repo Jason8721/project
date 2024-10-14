7b72a2812a7c118da746b4361eaede780a9be239
JasonEsFF <khanly033@gmail.com>
2024-10-14 23:08:58 +0330

    #

--- index.php
+++ index.php
@@ -6,24 +6,51 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
-    <title>GameLibrary.com</title>
+    <title>GameLibrary.ir</title>
    
 </head>
-<body class="p-3 mb-2 bg-dark-subtle text-dark-emphasis">
+<body class=" mb-2 bg-dark-subtle text-dark-emphasis">
   
-  <div class="container">
+<nav class="navbar navbar-expand-lg " dir="ltr" >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">GameL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#" >صفحه اصلی</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">کتابخاته</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            دسته بندی ها
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">اکشن</a></li>
+            <li><a class="dropdown-item" href="#">ترسناک</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">همه دسته بندی ها</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">تماس با ما</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
    
-  <header>
-        <h1>وبسایت من</h1>
-        <nav>
-            <ul>
-                <li><a href="#about">درباره ما</a></li>
-                <li><a href="#features">ویژگی‌ها</a></li>
-                <li><a href="#news">اخبار</a></li>
-                <li><a href="#contact">تماس با ما</a></li>
-            </ul>
-        </nav>
-    </header>
+
+  
+    
     <section id="about">
         <h2>درباره ما</h2>
         <p>ما یک وبسایت برای ارائه بهترین خدمات به کاربران هستیم.</p>
@@ -33,7 +60,7 @@
         <ul>
             <li>ویژگی ۱: خدمات باکیفیت</li>
             <li>ویژگی ۲: پشتیبانی ۲۴ ساعته</li>
-            <li>ویژگی ۳: قیمت مناسب</li>
+            <li>ویژگی ۳: انواع بازی ها </li>
         </ul>
     </section>
     <section id="news">
@@ -52,7 +79,7 @@
     <footer>
         <p>کپی‌رایت © 2024 وبسایت من</p>
     </footer>
-  </div>
+ 
 
   
 
